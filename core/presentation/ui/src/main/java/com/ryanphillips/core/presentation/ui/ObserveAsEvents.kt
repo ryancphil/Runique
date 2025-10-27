253c2d055d94a863e69c61ca99515602fdcb2dc6
ryancphil <ryancphil92@gmail.com>
2025-10-27 10:54:30 -0500

    Setup ObserveAsEvents utility.

---
+++ core/presentation/ui/src/main/java/com/ryanphillips/core/presentation/ui/ObserveAsEvents.kt
@@ -0,0 +1,33 @@
+package com.ryanphillips.core.presentation.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+
+/**
+ * This composable function lets us handle One-Time Events for
+ * things like Snackbars that are not persistent UI State and should
+ * only be handled once. You wouldn't want to retrigger a snackbar
+ * on device rotations, for example.
+ */
+@Composable
+fun <T> ObserveAsEvents(
+    flow: Flow<T>,
+    key1: Any? = null,
+    key2: Any? = null,
+    onEvent: (T) -> Unit
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(flow, lifecycleOwner.lifecycle, key1, key2) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) { // Key line for avoiding missed emissions!
+                flow.collect(onEvent)
+            }
+        }
+    }
+}
