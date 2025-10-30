1a1b5f8cd65c4fafa0867774e9ea92ff5dbc43c1
ryancphil <ryancphil92@gmail.com>
2025-10-30 18:07:13 -0500

    Sync pending runs

--- core/database/src/main/java/com/ryanphillips/core/database/RunDatabase.kt
+++ core/database/src/main/java/com/ryanphillips/core/database/RunDatabase.kt
@@ -3,13 +3,17 @@ package com.ryanphillips.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.ryanphillips.core.database.dao.RunDao
+import com.ryanphillips.core.database.dao.RunPendingSyncDao
+import com.ryanphillips.core.database.entity.DeletedRunSyncEntity
 import com.ryanphillips.core.database.entity.RunEntity
+import com.ryanphillips.core.database.entity.RunPendingSyncEntity
 
 @Database(
-    entities = [RunEntity::class],
+    entities = [RunEntity::class, RunPendingSyncEntity::class, DeletedRunSyncEntity::class],
     version = 1
 )
-abstract class RunDatabase: RoomDatabase() {
+abstract class RunDatabase : RoomDatabase() {
 
     abstract val runDao: RunDao
+    abstract val runPendingSyncDao: RunPendingSyncDao
 }
